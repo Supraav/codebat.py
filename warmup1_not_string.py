7932bf02262b97731973b03fed37f51afce4271d
suprav <47569979+Supraav@users.noreply.github.com>
2021-10-10 23:54:17 +0545

    Add files via upload

---
+++ warmup1_not_string.py
@@ -0,0 +1,15 @@
+# Given a string, return a new string where "not " has been added to the front. 
+# However, if the string already begins with "not", return the string unchanged.
+
+
+# not_string('candy') → 'not candy'
+# not_string('x') → 'not x'
+# not_string('not bad') → 'not bad'
+
+
+def not_string(str):
+  s=str.split()        #splits 'not candy' into list as ['not','candy] i.e [s[0],s[1]]
+  if s[0]!='not':
+    return "not "+str
+  else:
+    return str
