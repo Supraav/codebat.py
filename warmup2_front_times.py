d9f783fb547ca55b5f40959df00da25e7310fe85
Suprav <47569979+Supraav@users.noreply.github.com>
2021-10-13 20:19:00 +0545

    Add files via upload

---
+++ warmup2_front_times.py
@@ -0,0 +1,14 @@
+# Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+
+# string_times('Hi', 2) → 'HiHi'
+# string_times('Hi', 3) → 'HiHiHi'
+# string_times('Hi', 1) → 'Hi'
+
+
+str='hi'
+r=''
+for i in range(2):
+  r=r+str
+  
+print(r)
